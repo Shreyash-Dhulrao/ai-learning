956863f2713dd92e3de916740459ffb56ec2930a
Shreyash-Dhulrao <shreyashdhularao@gmail.com>
2026-09-25 14:56:13 +0530

    Operations on Array

--- Numpy/Day-1 25-09-26/first.py
+++ Numpy/Day-1 25-09-26/first.py
@@ -9,5 +9,11 @@ import numpy as np
 # print(one_d_array)
 
 # Two Dimensional Array
-two_d_array = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
-print(two_d_array)
+# two_d_array = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+# print(two_d_array)
+
+# Operations on an Array
+arr = np.array([1, 2, 3, 4])
+
+print(arr + 5) # Add 5 in each number of array
+print(arr * 2) # Multiply by 2 each number of array
