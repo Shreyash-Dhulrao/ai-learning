0aff618654a6b1ebd22e4ec2c7b170ef1af4c068
Shreyash-Dhulrao <shreyashdhularao@gmail.com>
2026-09-25 14:53:07 +0530

    One Dimensional Array

--- Numpy/Day-1 25-09-26/first.py
+++ Numpy/Day-1 25-09-26/first.py
@@ -1,5 +1,9 @@
 # First Program
 import numpy as np
 
-test_data = np.array((2,4,6,8,10))
-print(test_data)
+# test_data = np.array((2,4,6,8,10))
+# print(test_data)
+
+# One Dimensional Array
+one_d_array = np.array((1,2,3,4,5))
+print(one_d_array)
