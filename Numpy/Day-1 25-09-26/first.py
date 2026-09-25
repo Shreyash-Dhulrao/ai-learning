2555b7a965f481054062707fa374926b835abf1d
Shreyash-Dhulrao <shreyashdhularao@gmail.com>
2026-09-25 14:54:30 +0530

    Two  Dimensional Array

--- Numpy/Day-1 25-09-26/first.py
+++ Numpy/Day-1 25-09-26/first.py
@@ -5,5 +5,9 @@ import numpy as np
 # print(test_data)
 
 # One Dimensional Array
-one_d_array = np.array((1,2,3,4,5))
-print(one_d_array)
+# one_d_array = np.array((1,2,3,4,5))
+# print(one_d_array)
+
+# Two Dimensional Array
+two_d_array = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+print(two_d_array)
