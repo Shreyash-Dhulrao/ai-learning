2e8a153dfe6317b762d2a2c08d4d080424fa4cb3
Shreyash-Dhulrao <shreyashdhularao@gmail.com>
2026-09-25 15:01:39 +0530

    Basics of Arrays

--- Numpy/Day-1 25-09-26/first.py
+++ Numpy/Day-1 25-09-26/first.py
@@ -1,19 +1,19 @@
-# First Program
+# -------------- First Program --------------
 import numpy as np
 
 # test_data = np.array((2,4,6,8,10))
 # print(test_data)
 
-# One Dimensional Array
+# -------------- One Dimensional Array --------------
 # one_d_array = np.array((1,2,3,4,5))
 # print(one_d_array)
 
-# Two Dimensional Array
+# -------------- Two Dimensional Array --------------
 # two_d_array = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
 # print(two_d_array)
 
-# Operations on an Array
-arr = np.array([1, 2, 3, 4])
+# -------------- Operations on an Array --------------
+# arr = np.array([1, 2, 3, 4])
 
-print(arr + 5) # Add 5 in each number of array
-print(arr * 2) # Multiply by 2 each number of array
+# print(arr + 5) # Add 5 in each number of array
+# print(arr * 2) # Multiply by 2 each number of array
