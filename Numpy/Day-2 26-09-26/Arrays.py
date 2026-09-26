4a664dca9877e41f8c8a2b3d39d588e9c3f40621
Shreyash-Dhulrao <shreyashdhularao@gmail.com>
2026-09-26 13:12:17 +0530

    More Methods

--- Numpy/Day-2 26-09-26/Arrays.py
+++ Numpy/Day-2 26-09-26/Arrays.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 ''' Methods from which we can check about array in numpy
-1. np.shape: It is used to check the shape of the array (i.e. 2,3 or 3,4 etc)
-2. np.size: It is used to check the total count of the array 
-3. np.ndim: It is use to check the dimensions of the array (i.e. 1 or 2 or 3)
-4. np.dtype: It is use to check the data type present in the array (int, float, str etc)
+1. shape: It is used to check the shape of the array (i.e. 2,3 or 3,4 etc)
+2. size: It is used to check the total count of the array 
+3. ndim: It is use to check the dimensions of the array (i.e. 1 or 2 or 3)
+4. dtype: It is use to check the data type present in the array (int, float, str etc)
 '''
 
 # ----------- Size Method -----------
