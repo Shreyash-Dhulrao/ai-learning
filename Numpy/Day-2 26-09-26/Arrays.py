f77577df47c37d1ff2ff107c02ef186dd4fafd64
Shreyash-Dhulrao <shreyashdhularao@gmail.com>
2026-09-26 13:04:11 +0530

    Numpy Size

--- Numpy/Day-2 26-09-26/Arrays.py
+++ Numpy/Day-2 26-09-26/Arrays.py
@@ -6,3 +6,7 @@ import numpy as np
 3. np.ndim: It is use to check the dimensions of the array (i.e. 1 or 2 or 3)
 4. np.dtype: It is use to check the data type present in the array (int, float, str etc)
 '''
+
+# Size method
+arr = np.array([20,30,10,50,40])
+print(arr.size) # It will return 5 as the elements in the array is 5
