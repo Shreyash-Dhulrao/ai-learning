584111de52ede1d0f508f4ea121295c93242d079
Shreyash-Dhulrao <shreyashdhularao@gmail.com>
2026-09-26 13:09:17 +0530

    ndim Method

--- Numpy/Day-2 26-09-26/Arrays.py
+++ Numpy/Day-2 26-09-26/Arrays.py
@@ -7,10 +7,14 @@ import numpy as np
 4. np.dtype: It is use to check the data type present in the array (int, float, str etc)
 '''
 
-# Size Method
+# ----------- Size Method -----------
 # arr = np.array([20,30,10,50,40])
 # print(arr.size) # It will return 5 as the elements in the array is 5
 
-# Shape Method
+# ----------- Shape Method -----------
+# arr = np.array([[30,20,50],[10,40,60]])
+# print(arr.shape) # It will return 2,3 as it contains 2 rows and 3 columns
+
+# ----------- Ndim Method -----------
 arr = np.array([[30,20,50],[10,40,60]])
-print(arr.shape) # It will return 2,3 as it contains 2 rows and 3 columns
+print(arr.ndim) # It will return 2, because it is a 2d array
